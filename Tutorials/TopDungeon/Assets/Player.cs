6d447ac0e4564f2d6a2f0a84ba5ced7a46336ec9
Kevin Day <kday@up.com>
2021-11-12 18:57:58 -0600

    Collider stuff, note: make box2Dcollider be 1 pixel border inside sprite

--- Tutorials/TopDungeon/Assets/Player.cs
+++ Tutorials/TopDungeon/Assets/Player.cs
@@ -7,12 +7,17 @@ public class Player : MonoBehaviour
 {
     private static Vector3 face_right = new Vector3(1, 1, 1);
     private static Vector3 face_left = new Vector3(-1, 1, 1);
+    private static Vector2 Vertical = new Vector2(0, 1);
+    private static Vector2 Horizontal = new Vector2(1, 0);
+    private static float move_granularity = 0.01F;
 
     private BoxCollider2D box_collider;
     private Vector3 move_delta;
 
     private PlayerInput player_input;
     private InputAction move_action;
+
+    private RaycastHit2D hit_box;
 
     private void Awake()
     {
@@ -32,6 +37,9 @@ public class Player : MonoBehaviour
     {
         bool left_down = (move_delta.x < 0);
         bool right_down = (move_delta.x > 0);
+        bool up_down = (move_delta.y < 0);
+        bool down_down = (move_delta.y > 0);
+        float delta_time = Time.deltaTime;
 
         if (left_down)
         {
@@ -42,7 +50,42 @@ public class Player : MonoBehaviour
             transform.localScale = face_right;
         }
 
-        transform.Translate(move_delta * Time.deltaTime);
+        int horizontal_movement = (int)(move_delta.x * delta_time / move_granularity);
+        int vertical_movement = (int)(move_delta.y * delta_time / move_granularity);
+
+        hit_box = Physics2D.BoxCast(transform.position, box_collider.size, 0, Vertical, vertical_movement * move_granularity, LayerMask.GetMask("Actor", "Blocking"));
+        if(hit_box.collider == null)
+        {
+            transform.Translate(0, vertical_movement * move_granularity, 0);
+        }
+        else if(up_down)
+        {
+            transform.position = new Vector3(transform.position.x, hit_box.collider.gameObject.transform.position.y + 0.16f);
+        }
+        else if(down_down)
+        {
+            transform.position = new Vector3(transform.position.x, hit_box.collider.gameObject.transform.position.y - 0.16f);
+        }
+
+        hit_box = Physics2D.BoxCast(transform.position, box_collider.size, 0, Horizontal, horizontal_movement * move_granularity, LayerMask.GetMask("Actor", "Blocking"));
+        if (hit_box.collider == null)
+        {
+            transform.Translate(horizontal_movement * move_granularity, 0, 0);
+        }
+        else if (left_down)
+        {
+            Debug.Log(string.Format("Before ({0:F3},{1:F3})", transform.position.x, transform.position.y));
+            Debug.Log(string.Format("Collided with {0:F3}", hit_box.collider.gameObject.transform.position.x));
+            transform.position = new Vector3(hit_box.collider.gameObject.transform.position.x + 0.16f, transform.position.y);
+            Debug.Log(string.Format("After ({0:F3},{1:F3})", transform.position.x, transform.position.y));
+        }
+        else if (right_down)
+        {
+            Debug.Log(string.Format("Before ({0:F3},{1:F3})", transform.position.x, transform.position.y));
+            Debug.Log(string.Format("Collided with {0:F3}", hit_box.collider.gameObject.transform.position.x));
+            transform.position = new Vector3(hit_box.collider.gameObject.transform.position.x - 0.16f, transform.position.y);
+            Debug.Log(string.Format("After ({0:F3},{1:F3})", transform.position.x, transform.position.y));
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
